62c6ed0214114f7ff4199827236f33ed7d35f8b1
issue4th <44305925+issue4th@users.noreply.github.com>
2021-12-19 11:18:18 +0000

    debugging

--- main.ts
+++ main.ts
@@ -70,6 +70,35 @@ function start_fireballs () {
         true
         )
     }
+    for (let location of tiles.getTilesByType(assets.tile`myTile10`)) {
+        temporary = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . 4 4 4 4 4 4 4 4 4 4 4 . . . 
+            . . 4 4 4 4 4 4 4 4 4 4 4 . . . 
+            . . 4 4 5 5 1 1 1 1 1 4 4 . . . 
+            . . 4 4 5 5 1 1 1 1 1 4 4 . . . 
+            . . 4 4 5 5 1 1 1 1 1 4 4 . . . 
+            . . 4 4 5 5 1 1 1 1 1 4 4 . . . 
+            . . 4 4 5 5 1 1 1 1 1 4 4 . . . 
+            . . 4 4 5 5 5 5 5 5 5 4 4 . . . 
+            . . 4 4 5 5 5 5 5 5 5 4 4 . . . 
+            . . 4 4 4 4 4 4 4 4 4 4 4 . . . 
+            . . 4 4 4 4 4 4 4 4 4 4 4 . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Fireball)
+        temporary.startEffect(effects.fire)
+        tiles.placeOnTile(temporary, location)
+        tiles.setTileAt(location, assets.tile`tile0`)
+        animation.runMovementAnimation(
+        temporary,
+        "c 0 150 0 -150 0 0",
+        2000,
+        true
+        )
+    }
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Hops_and_Paw.vy == 0) {
@@ -303,6 +332,9 @@ function start_coins () {
         true
         )
     }
+    for (let temporary of sprites.allOfKind(SpriteKind.gem_coin)) {
+        temporary.destroy()
+    }
     for (let location of tiles.getTilesByType(assets.tile`myTile13`)) {
         temporary = sprites.create(img`
             . . . . . . . . . . . . . . . . 
@@ -483,6 +515,9 @@ function start_coins () {
         100,
         true
         )
+    }
+    for (let temporary of sprites.allOfKind(SpriteKind.draconium_coin)) {
+        temporary.destroy()
     }
     for (let location of tiles.getTilesByType(assets.tile`myTile17`)) {
         temporary = sprites.create(img`
@@ -742,40 +777,6 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.gem_coin, function (sprite, othe
     info.changeScoreBy(5)
     otherSprite.destroy()
 })
-function start_fireballs2 () {
-    for (let temporary of sprites.allOfKind(SpriteKind.Fireball)) {
-        temporary.destroy()
-    }
-    for (let location of tiles.getTilesByType(assets.tile`myTile10`)) {
-        temporary = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . 4 4 4 4 4 4 4 4 4 4 4 . . . 
-            . . 4 4 4 4 4 4 4 4 4 4 4 . . . 
-            . . 4 4 5 5 1 1 1 1 1 4 4 . . . 
-            . . 4 4 5 5 1 1 1 1 1 4 4 . . . 
-            . . 4 4 5 5 1 1 1 1 1 4 4 . . . 
-            . . 4 4 5 5 1 1 1 1 1 4 4 . . . 
-            . . 4 4 5 5 1 1 1 1 1 4 4 . . . 
-            . . 4 4 5 5 5 5 5 5 5 4 4 . . . 
-            . . 4 4 5 5 5 5 5 5 5 4 4 . . . 
-            . . 4 4 4 4 4 4 4 4 4 4 4 . . . 
-            . . 4 4 4 4 4 4 4 4 4 4 4 . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.Fireball)
-        temporary.startEffect(effects.fire)
-        tiles.placeOnTile(temporary, location)
-        tiles.setTileAt(location, assets.tile`tile0`)
-        animation.runMovementAnimation(
-        temporary,
-        "c 0 150 0 -150 0 0",
-        2000,
-        true
-        )
-    }
-}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Fireball, function (sprite, otherSprite) {
     info.changeLifeBy(-2)
     otherSprite.destroy()
@@ -799,7 +800,6 @@ function startLevel () {
     hide_start_position()
     start_torches()
     start_fireballs()
-    start_fireballs2()
     start_flowers()
     start_coins()
 }
@@ -941,7 +941,7 @@ scene.setBackgroundImage(img`
     6666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666
     6666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666
     `)
-current_level = 4
+current_level = 1
 Hops_and_Paw = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
